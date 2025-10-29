7815c9caeb1087e1e0428b6e09983ffd87e281a0
mathftz <mathieu.frantz@gmail.com>
2025-10-29 21:48:32 +0100

    feat: juste prix

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+
+# Générer un nombre aléatoire entre 1 et 1000
+nombre_a_trouver = random.randint(1, 101)
+
+trouve = False
+nb_tentatives = 0
+
+while not trouve:
+    # Demander à l'utilisateur de deviner le nombre
+    nombre_saisis = int(input("Devinez le nombre entre 1 et 100 : "))
+
+    # Incrémenter le nombre de tentatives de 1
+    nb_tentatives = nb_tentatives + 1
+
+    if nombre_saisis > nombre_a_trouver:
+        print("Le nombre saisi est trop grand")
+
+    elif nombre_saisis == nombre_a_trouver:
+        print(f"Gagné en {nb_tentatives} tentatives")
+        trouve = True
+
+    else:
+        print("Le nombre saisi est trop petit")
+
+print("Fin du jeu")
