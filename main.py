300f16da09242c1359d1bee4cf4a160fa2a80c42
mathftz <mathieu.frantz@gmail.com>
2025-10-29 22:10:26 +0100

    feat: juste prix v2

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
 
-# Générer un nombre aléatoire entre 1 et 1000
-nombre_a_trouver = random.randint(1, 101)
+# Générer un nombre aléatoire entre 1 et 10000
+nombre_a_trouver = random.randint(1, 10001)
 
 trouve = False
 nb_tentatives = 0
 
 while not trouve:
     # Demander à l'utilisateur de deviner le nombre
-    nombre_saisis = int(input("Devinez le nombre entre 1 et 100 : "))
+    nombre_saisis = int(input("Devinez le nombre entre 1 et 10000 : "))
 
     # Incrémenter le nombre de tentatives de 1
     nb_tentatives = nb_tentatives + 1
